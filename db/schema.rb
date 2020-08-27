d31149acee84379900833729bd0fa6f2c90770a8
rgalazb <rgalaz@buk.cl>
2020-08-26 23:23:06 -0400

    fixup! crear modelo para comentarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_031441) do
+ActiveRecord::Schema.define(version: 2020_08_27_031649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "request_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["request_id"], name: "index_comments_on_request_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "jwt_blacklists", force: :cascade do |t|
     t.string "jti"
@@ -44,5 +54,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_031441) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "requests"
+  add_foreign_key "comments", "users"
   add_foreign_key "requests", "users"
 end
